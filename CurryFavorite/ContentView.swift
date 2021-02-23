c130b94a32ec646e1b2aaf5649c2169d81029f1a
ketch824 <ketch824@hotmail.com>
2021-02-23 16:08:22 +0900

    プロジェクト作成

---
+++ CurryFavorite/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CurryFavorite
+//
+//  Created by 畠山真一 on 2021/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CurryFavoriteTests/CurryFavoriteTests.swift
@@ -0,0 +1,33 @@
+//
+//  CurryFavoriteTests.swift
+//  CurryFavoriteTests
+//
+//  Created by 畠山真一 on 2021/02/23.
+//
+
+import XCTest
+@testable import CurryFavorite
+
+class CurryFavoriteTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
